5af6f559e24b4f15b278674850dea20ba6be1396
GustavoCruz09 <dacruz.gustavo09@gmail.com>
2023-05-04 11:56:16 -0300

    first commit

---
+++ src/controller/ArquivosController.java
@@ -0,0 +1,112 @@
+package controller;
+
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
+
+public class ArquivosController implements IArquivosController {
+
+	public ArquivosController() {
+		super();
+	}
+
+	@Override
+	public void readDir(String path) throws IOException {
+		File dir = new File(path); // path é o caminho do diretório
+		if (dir.exists() && dir.isDirectory()) { // verifica se existe e é um diretório
+			File[] files = dir.listFiles(); // cria um vetor com todos os arquivos e diretórios
+			for (File f : files) {
+				if (f.isFile()) {
+					System.out.println("      \t" + f.getName());
+				} else {
+					System.out.println("<DIR>\t" + f.getName());
+				}
+			}
+		} else {
+			throw new IOException("Diretório Inválido");
+		}
+
+	}
+
+	@Override
+	public void createFile(String path, String nome) throws IOException {
+		File dir = new File(path);
+		File arq = new File(path, nome + ".txt");
+		if (dir.exists() && dir.isDirectory()) {
+			boolean existe = false;
+			if (arq.exists()) {
+				existe = true;
+			}
+			String conteudo = geraTxt();
+			FileWriter fileWriter = new FileWriter(arq, existe); // responsavel por abrir arquivo e definir se será
+																	// appendu write
+			PrintWriter print = new PrintWriter(fileWriter);
+			print.write(conteudo);
+			print.flush();
+			print.close();
+			fileWriter.close();
+		} else {
+			throw new IOException("Diretório Inválido");
+		}
+
+	}
+
+	private String geraTxt() {
+		StringBuffer buffer = new StringBuffer();
+		String linha = "";
+		while (!linha.equalsIgnoreCase("fim")) {
+			linha = JOptionPane.showInputDialog(null, "Digite uma frase", "Entrada de texto",
+					JOptionPane.INFORMATION_MESSAGE);
+			if (!linha.equalsIgnoreCase("fim")) {
+				buffer.append(linha + "\r\n");
+			}
+		}
+		return buffer.toString();
+	}
+
+	@Override
+	public void readFile(String path, String nome) throws IOException {
+		File arq = new File(path, nome);
+		if (arq.exists() && arq.isFile()) {
+			FileInputStream fluxo = new FileInputStream(arq);
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			while (linha != null) { // procurando end of file EOF
+				if(linha.contains("Fruits")) {
+					Tabulacao(linha);
+				}
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+		} else {
+			throw new IOException("Arquivo Inválido");
+		}
+	}
+
+	private void Tabulacao(String linha) {
+		String[] vetorsplit = linha.split(",");
+		 
+		System.out.println(vetorsplit[0] + "\t" + vetorsplit[1] + "\t" + vetorsplit[3]); // food name tab scientific name tab subgroup
+	}
+
+	@Override
+	public void openFile(String path, String nome) throws IOException {
+		File arq = new File(path, nome);
+		if(arq.exists() && arq.isFile()) {
+			Desktop desktop = Desktop.getDesktop();
+			desktop.open(arq);
+		} else {
+			throw new IOException("Arquivo Inválido");
+		}
+	}
+
+}
